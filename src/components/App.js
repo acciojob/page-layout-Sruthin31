ab8d692b27ade077f1ad1116a5e3195cc05a1d88
Sruthin31 <nsruthi12@gmail.com>
2026-03-26 15:24:56 +0530

    Update App and PageLayout, configure webpack

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <PageLayout header={<h1>Welcome to my website</h1>} footer={<p>© 2023 My Website.All rights reserved.</p>}>
+        <p>This is the content of my webstie.</p>
+        </PageLayout>
     </div>
   )
 }
---
+++ src/components/PageLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+const PageLayout = ({header,footer,children}) =>{
+    return(
+        <div>
+        {header}
+        {children}
+        {footer}
+        </div>
+    )
+
+}
+export default PageLayout
